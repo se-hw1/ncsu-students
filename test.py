c45d4440e6409c78989f1f776e82f8dd55f926b5
sasidharappalla <120258330+sasidharappalla@users.noreply.github.com>
2024-08-29 13:44:22 -0400

    Update test.py

--- test.py
+++ test.py
@@ -5,5 +5,9 @@ from myfile import find_maximum, num_even, greetingpeople
 def test_greet():
     assert greetingpeople("Alice") == "Hello, Alice!"  # This should pass
 
+def test_num_even():
+
+    assert num_even(4) == True #This will pass 4 = even
+
 def test_find_max():
     assert find_maximum(10, 5) == 10  # This will fail due to the intentional error
