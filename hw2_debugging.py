647cf8c6b0d486646e366fa2ab9e28e4dfbe5cc6
navyatej3112 <navcs24@gmail.com>
2024-09-11 03:10:35 -0400

    Update hw2_debugging.py

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -11,22 +11,22 @@ def merge_sort(arr):
 def recombine(left_arr, right_arr):
     left_index = 0
     right_index = 0
-    mergeArr = [None] * (len(left_arr) + len(right_arr))
+    merge_arr = [None] * (len(left_arr) + len(right_arr))
     while left_index < len(left_arr) and right_index < len(right_arr):
         if left_arr[left_index] < right_arr[right_index]:
             right_index += 1
-            mergeArr[left_index + right_index] = left_arr[left_index]
+            merge_arr[left_index + right_index] = left_arr[left_index]
         else:
             left_index += 1
-            mergeArr[left_index + right_index] = right_arr[right_index]
+            merge_arr[left_index + right_index] = right_arr[right_index]
 
     for i in range(right_index, len(right_arr)):
-        mergeArr[left_index + right_index] = right_arr[i]
+        merge_arr[left_index + right_index] = right_arr[i]
     
     for i in range(left_index, len(left_arr)):
-        mergeArr[left_index + right_index] = left_arr[i]
+        merge_arr[left_index + right_index] = left_arr[i]
 
-    return mergeArr
+    return merge_arr
 
 arr = rand.random_array([None] * 20)
 arr_out = merge_sort(arr)
