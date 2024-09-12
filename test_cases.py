706edf00246046d6224b8a8b3f561c112f38a070
navyatej3112 <navcs24@gmail.com>
2024-09-11 20:03:52 -0400

    Update test_cases.py

--- test_cases.py
+++ test_cases.py
@@ -1,16 +1,16 @@
 from hw2_debugging import merge_sort
 
-
 def test_correctly_sorted():
-    arr = [1, 5, 4, 3, 2]
-    assert merge_sort(arr) == [1, 2, 3, 4, 5]
+    
+    assert merge_sort([1,5,4,3,2]) == [1,2,3,4,5]
 
 
 def test_jumbled_order_or_correct_order():
-    arr = [1, 2, 3, 4, 5]
-    assert merge_sort(arr) == [1, 2, 3, 4, 5]
+    
+    assert merge_sort([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
 
 
-def check_sorting_negative_numbers():
-    arr = [-5, -6, -1, -2, -3, -4]
-    assert merge_sort(arr) == [-6, -5, -4, -3, -2, -1]
+def test_single_value():
+     
+    assert merge_sort([7]) == [7]
+
