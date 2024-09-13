b02c196f93b9625616a8fb4ea24c2a9c9f033906
navyatej3112 <navcs24@gmail.com>
2024-09-12 20:31:20 -0400

    Update rand.py

--- rand.py
+++ rand.py
@@ -1,25 +1,12 @@
-"""
-This module contains utilities for generating random arrays.
-"""
+rand.py
+
 
 import subprocess
 
 def random_array(arr):
-    """
-    Fills the input array with random integers between 1 and 20.
-
-    Args:
-        arr (list): A list to be filled with random integers.
-
-    Returns:
-        list: The input list filled with random integers.
-    """
-    for i, _ in enumerate(arr):
-        shuffled_num = subprocess.run(
-            ["shuf", "-i1-20", "-n1"],
-            capture_output=True,
-            check=True  # Ensure subprocess exits with success
-        )
+    shuffled_num = None
+    for i in range(len(arr)):
+        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
         arr[i] = int(shuffled_num.stdout)
     return arr
 
